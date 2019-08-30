28ca528f4104d9e049a4945c3f2a7fce07bf1b70
technology <technology@CPX-APR4FFVCHMH.dir.svc.accenture.com>
2019-08-29 21:00:29 -0500

    rentacar2

---
+++ RentaCar2/src/main/java/com/accenture/RentaCar2/app/RentaCar2Application.java
@@ -0,0 +1,13 @@
+package com.accenture.RentaCar2.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaCar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaCar2Application.class, args);
+	}
+
+}
